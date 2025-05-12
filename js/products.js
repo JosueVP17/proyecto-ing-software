e0d4762ed01e493aa1031fb7eaed1cac6d023650
Jos <jos.val.pe07@gmail.com>
2025-05-12 13:38:20 -0600

    [ADDED] Edit button for products via administrator

--- js/products.js
+++ js/products.js
@@ -1,8 +1,9 @@
 import {db} from "./firebase-config.js"
-import {collection, addDoc, getDoc, getDocs, query, where} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js"
+import {collection, doc, addDoc, getDoc, getDocs, query, where, updateDoc, deleteDoc} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js"
 
 const productsCollection = collection(db, 'products')
 
+// OBTENER PRODUCTOS DE FIREBASE
 const getProducts = async() => {
     try {
         const products = await getDocs(productsCollection)
@@ -16,6 +17,7 @@ const getProducts = async() => {
     }
 }
 
+// CARGAR PRODUCTOS DEL CATÁLOGO
 const renderProducts = async() => {
     const productsContainer = document.getElementById('productos')
     productsContainer.innerHTML = ''
@@ -38,22 +40,30 @@ const renderProducts = async() => {
     })
 }
 
+// DISPLAY DE MODAL PARA AÑADIR PRODUCTO
+const addProductBtn = document.getElementById('addProductBtn')
+const addModalCloseBtn = document.getElementById('addModalCloseBtn')
+const modalAgregar = document.getElementById('modalAgregar')
+
 addProductBtn.addEventListener('click', () => {
-    adminModal.style.display = 'block'
+    modalAgregar.style.display = 'block'
 })
 
-closeModalBtn.addEventListener('click', () => {
-    adminModal.style.display = 'none'
+addModalCloseBtn.addEventListener('click', () => {
+    modalAgregar.style.display = 'none'
 })
 
-productForm.addEventListener('submit', async (e) => {
+// OBTENER FORMULARIO Y AÑADIR PRODUCTO
+const addProductForm = document.getElementById('addProductForm')
+
+addProductForm.addEventListener('submit', async (e) => {
     e.preventDefault()
 
-    const nombre = document.getElementById('nombre').value
-    const precio = parseFloat(document.getElementById('precio').value)
-    const marca = document.getElementById('marca').value
-    const imagen = document.getElementById('imagen').value
-    const descripcion = document.getElementById('descripcion').value
+    const nombre = document.getElementById('addNombre').value
+    const precio = parseFloat(document.getElementById('addPrecio').value)
+    const marca = document.getElementById('addMarca').value
+    const imagen = document.getElementById('addImagen').value
+    const descripcion = document.getElementById('addDescripcion').value
 
     const producto = { nombre, precio, marca, imagen, descripcion }
 
@@ -67,16 +77,79 @@ productForm.addEventListener('submit', async (e) => {
 
     try {
         const newDoc = await addDoc(productsCollection, producto)
-        renderProducts()
+        modalAgregar.style.display = 'none'
+        document.getElementById('addProductForm').reset()
+
+        await renderProducts()
+        await renderTable()
         console.log('@@@ Producto agregado con ID ', newDoc.id)
-        adminModal.style.display = 'none'
-        document.getElementById('productForm').reset()
     } catch(error) {
         console.log('@@@ Error al agregar el producto: ', error)
     }
 })
 
-inventoryBtn.addEventListener('click', async() => {
+const renderTable = async () => {
+    const tbody = document.getElementById('tbody')
+    const products = await getProducts()
+
+    tbody.innerHTML = ''
+
+    products.forEach((product) => {
+        const row = document.createElement('tr')
+
+        row.innerHTML = `
+            <td>${product.id}</td>
+            <td>${product.nombre}</td>
+            <td>$${product.precio}</td>
+            <td>${product.marca}</td>
+            <td><img src="${product.imagen}"></td>
+            <td>${product.descripcion}</td>
+            <td>
+                <button class="productEdit edit" data-id=${product.id}><i class="fa fa-pencil" aria-hidden="true"></i> Editar</button>
+                <button class="productDelete delete" data-id=${product.id}><i class="fa fa-trash" aria-hidden="true"></i> Eliminar</button>
+            </td>
+        `
+
+        tbody.appendChild(row)
+    })
+
+    // CARGA DE ATRIBUTOS AL MODAL DE ACTUALIZAR PRODUCTO
+    document.querySelectorAll('.edit').forEach((btn) => {
+        btn.addEventListener('click', async(event) => {
+            modalActualizar.style.display = 'block'
+
+            const productId = event.target.getAttribute('data-id')
+            const productRef = doc(db, 'products', productId)
+            const productSnap = await getDoc(productRef)
+            const productData = productSnap.data()
+
+            const id = document.getElementById('productID')
+            const nombre = document.getElementById('updateNombre')
+            const precio = document.getElementById('updatePrecio')
+            const marca = document.getElementById('updateMarca')
+            const imagen = document.getElementById('updateImagen')
+            const descripcion = document.getElementById('updateDescripcion')
+
+            id.value = productId
+            nombre.value = productData.nombre
+            precio.value = productData.precio
+            marca.value = productData.marca
+            imagen.value = productData.imagen
+            descripcion.value = productData.descripcion
+        })
+    })
+
+    // CERRAR MODAL PARA ACTUALIZAR PRODUCTO
+    const updateModalCloseBtn = document.getElementById('updateModalCloseBtn')
+    const modalActualizar = document.getElementById('modalActualizar')
+
+    updateModalCloseBtn.addEventListener('click', () => {
+        modalActualizar.style.display = 'none'
+    })
+}
+
+// DISPLAY DE LA TABLA DE INVENTARIO
+inventoryBtn.addEventListener('click', async () => {
     const inventoryBtn = document.getElementById('inventoryBtn')
     const productsContainer = document.getElementById('productos')
     const table = document.getElementById('tabla')
@@ -86,33 +159,38 @@ inventoryBtn.addEventListener('click', async() => {
         table.style.display = "none"
         inventoryBtn.innerHTML = '<i class="fa fa-archive" aria-hidden="true"></i> INVENTARIO'
 
-        renderProducts()
+        await renderProducts()
     } else {
         productsContainer.style.display = "none"
         table.style.display = "block"
         inventoryBtn.innerHTML = '<i class="fa fa-long-arrow-left" aria-hidden="true"></i> REGRESAR'
 
-        const tbody = document.getElementById('tbody')
-        const products = await getProducts()
+        await renderTable()
+    }
+})
 
-        products.forEach((product) => {
-            const row = document.createElement('tr')
+// OBTENER FORMULARIO Y ACTUALIZAR PRODUCTO
+updateProductForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
 
-            row.innerHTML = `
-                <td>${product.id}</td>
-                <td>${product.nombre}</td>
-                <td>$${product.precio}</td>
-                <td>${product.marca}</td>
-                <td><img src="${product.imagen}"></td>
-                <td>${product.descripcion}</td>
-                <td>
-                    <button class="productEdit"><i class="fa fa-pencil" aria-hidden="true"></i> Editar</button>
-                    <button class="productDelete"><i class="fa fa-trash" aria-hidden="true"></i> Eliminar</button>
-                </td>
-            `
+    const id = document.getElementById('productID').value
+    const nombre = document.getElementById('updateNombre').value
+    const precio = document.getElementById('updatePrecio').value
+    const marca = document.getElementById('updateMarca').value
+    const imagen = document.getElementById('updateImagen').value
+    const descripcion = document.getElementById('updateDescripcion').value
+    
+    try {
+        const productRef = doc(db, 'products', id)
+        await updateDoc(productRef, {nombre, precio, marca, imagen, descripcion})
 
-            tbody.appendChild(row)
-        })
+        modalActualizar.style.display = 'none'
+        document.getElementById('updateProductForm').reset()
+        
+        await renderTable()
+        console.log('@@@ Producto actualizado')
+    } catch(error) {
+        console.log('@@@ Error al actualizar el producto', error)
     }
 })
 
