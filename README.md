2825fa2ae9621f96c4afb67851262c3ce0a5e9ef
Jos <jos.val.pe07@gmail.com>
2025-05-27 11:41:40 -0600

    [ADDED] Readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+npm install node
+npm install express
+npm install stripe
+npm install dotenv
+Add test key for stripe
+node server/stripe.js
--- server/stripe.js
+++ server/stripe.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
